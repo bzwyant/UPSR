809c6997360ff1a120cb077ffcaddaadae438cb1
bzwyant <bzwyant@gmail.com>
2025-08-06 10:24:18 -0500

    test download

--- scripts/data_preparation/download_galaxy_mnist.py
+++ scripts/data_preparation/download_galaxy_mnist.py
@@ -4,17 +4,24 @@ from galaxy_datasets import galaxy_mnist
 
 script_dir = Path(__file__)
 root = script_dir.parent.parent.parent  # Go up to the root of the project
-data_path = root / 'basicsr' / 'datasets' / 'galaxy_mnist' / 'train'
-data_path.mkdir(parents=True, exist_ok=True)
 
-catalog, label_cols = galaxy_mnist(
-    root=data_path,
-    train=True,
-    download=True
-)
+def download_data(phase='train'):
+    """Download Galaxy MNIST dataset."""
+    data_path = root / 'basicsr' / 'datasets' / 'galaxy_mnist' / phase
+    data_path.mkdir(parents=True, exist_ok=True)
 
-images_dir = data_path / 'images'
-for file_path in images_dir.iterdir():
-    file_path.rename(data_path / file_path.name)
+    catalog, label_cols = galaxy_mnist(
+        root=data_path,
+        train=(phase == 'train'),
+        download=True
+    )
 
-os.rmdir(images_dir)  # Remove the empty images directory
+    images_dir = data_path / 'images'
+    for file_path in images_dir.iterdir():
+        file_path.rename(data_path / file_path.name)
+
+    os.rmdir(images_dir)  # Remove the empty images directory
+
+if __name__ == '__main__':
+    # download_data('train')
+    download_data('gt')
