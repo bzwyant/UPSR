4ca2441c38ee93c8a8d27f5d1425ca3ae0e7b4bf
bzwyant <bzwyant@gmail.com>
2025-08-08 15:37:26 -0500

    wrong directory

--- scripts/jobs/crop_and_process_galaxy_mnist.sh
+++ scripts/jobs/crop_and_process_galaxy_mnist.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=galaxy_mnist_processing
+#SBATCH --partition=short
 #SBATCH -A e32704   # Account name
 #SBATCH -p gengpu   # GPU Partition
 #SBATCH --gres=gpu:a100:1   # Request 1 A100 GPU
@@ -7,7 +8,7 @@
 #SBATCH -n 1    # Number of tasks
 #SBATCH -t 1:00:00  # Max runtime
 #SBATCH --mem=16G   # Memory allocation
-#SBATCH --output=train_%j.log   # Log file (SLURM_JOB_ID included)
+#SBATCH --output=crop_%j.log   # Log file (SLURM_JOB_ID included)
 
 
 module load mamba
@@ -23,7 +24,7 @@ PATH_TO_DATASET="basicsr/datasets/galaxy_mnist"
 
 # Run image processing script and log output
 cd $PATH_TO_ROOT &&
-time python scripts/jobs/crop_galaxy_mnist.py \
+time python scripts/data_preparation/crop_galaxy_mnist.py \
     --input_dir "${PATH_TO_DATASET}/train" \
     --output-128 "${PATH_TO_DATASET}/train/gt" \
     --output-64 "${PATH_TO_DATASET}/train/lq" \
