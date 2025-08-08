31692eaceaa208f455f337500935e0aef98be4b2
bzwyant <bzwyant@gmail.com>
2025-08-08 15:46:20 -0500

    updated scripts

--- scripts/jobs/crop_and_process_galaxy_mnist.sh
+++ scripts/jobs/crop_and_process_galaxy_mnist.sh
@@ -11,19 +11,18 @@
 #SBATCH --output=crop_%j.log   # Log file (SLURM_JOB_ID included)
 
 
-module load mamba
-
 # Ensure real-time logging
 export PYTHONUNBUFFERED=1
 
 # Activate virtual environment
+module load mamba
+mamba init
 mamba activate UPSR
 
 # get path to root folder
 PATH_TO_DATASET="basicsr/datasets/galaxy_mnist"
 
 # Run image processing script and log output
-cd $PATH_TO_ROOT &&
 time python scripts/data_preparation/crop_galaxy_mnist.py \
     --input_dir "${PATH_TO_DATASET}/train" \
     --output-128 "${PATH_TO_DATASET}/train/gt" \
